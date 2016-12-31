4def708a5583f5c6ae02cacac22ea4b4f8ccafaa
amit-ros <854151@QJ07302866>
2016-12-31 14:17:13 -0600

    Updated Agent Master with the latest code.

--- ResortReservation/ResortManager/MasterUI/AgentMaster.aspx
+++ ResortReservation/ResortManager/MasterUI/AgentMaster.aspx
@@ -8,6 +8,17 @@
     <link rel="stylesheet" type="text/css" media="all" href="../css/pageheader.css" />    
     <link rel="stylesheet" type="text/css" media="all" href="../style.css" />    
     <title>Agent Master</title>
+    <style type="text/css">
+        .auto-style1
+        {
+            height: 23px;
+        }
+        .auto-style2
+        {
+            width: 120px;
+            height: 23px;
+        }
+    </style>
 </head>
 <body>
     <form id="form1" runat="server">
@@ -18,8 +29,8 @@
     <ContentTemplate>                                
     <div id="gridsection" class="gridsection">
         <asp:DataGrid ID="dgAgents" runat="server" AutoGenerateColumns="False" CellPadding="4"
-        DataKeyField="AgentID" ForeColor="#333333" GridLines="None" OnDeleteCommand="dgAgents_DeleteCommand"
-        OnSelectedIndexChanged="dgAgents_SelectedIndexChanged" Width="525px" BorderStyle="Ridge">
+        DataKeyField="AgentID" ForeColor="#333333" GridLines="None" OnDeleteCommand="dgAgents_ItemCommand"
+        OnSelectedIndexChanged="dgAgents_SelectedIndexChanged" Width="525px" BorderStyle="Ridge" OnItemCommand="dgAgents_ItemCommand">
         <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
         <EditItemStyle BackColor="#2461BF" />
         <SelectedItemStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
@@ -32,6 +43,7 @@
             <asp:BoundColumn DataField="AgentCode" HeaderText="Agent Code"></asp:BoundColumn>
             <asp:ButtonColumn CommandName="Select" HeaderText="[...]" Text="Edit"></asp:ButtonColumn>
             <asp:ButtonColumn CommandName="Delete" HeaderText="[...]" Text="Delete" Visible="False"></asp:ButtonColumn>
+              <asp:ButtonColumn CommandName="MaptoRate" HeaderText="[...]" Text="Map" ></asp:ButtonColumn>
         </Columns>
         </asp:DataGrid>
     </div>
@@ -49,12 +61,82 @@
             <asp:TextBox cssclass="input"  ID="txtAgentCode" runat="server"></asp:TextBox></td>
     </tr>
     <tr>
-        <td>
+        <td class="auto-style1">
             Agent Email Id</td>
-        <td style="width: 120px">
+        <td class="auto-style2">
             <asp:TextBox cssclass="input"  ID="txtAgentEmailId" runat="server" MaxLength="50" Width="200px" AutoCompleteType="Email"></asp:TextBox></td>
     </tr>
+        <tr>
+            <td class="auto-style1">Phone</td>
+            <td class="auto-style2">
+                <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="reqftxtBillingAddress0" runat="server" ControlToValidate="txtPhone" ErrorMessage="*" ForeColor="Red" ValidationGroup="valGroupRegister"></asp:RequiredFieldValidator>
+            </td>
+        </tr>
+       <tr>
+           <td>Password</td>
+           <td><asp:TextBox cssclass="input"  ID="txtPassword" TextMode="Password" runat="server"></asp:TextBox>
+           </td>
+
+       </tr>
     </table>
+   <table id="tblDetails">
+                        <tr>
+
+                            <td>Billing Address</td>
+                            <td>
+
+                                <asp:TextBox ID="txtBillingAddress" runat="server"></asp:TextBox>
+                                <asp:RequiredFieldValidator ID="reqftxtBillingAddress" runat="server" ControlToValidate="txtBillingAddress" ErrorMessage="*" ForeColor="Red" ValidationGroup="valGroupRegister"></asp:RequiredFieldValidator>
+
+                            </td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+                            <td>Payment Method</td>
+                            <td>
+                                <asp:DropDownList ID="ddlpaymentMethod"  runat="server">
+                                    <asp:ListItem>-Select-</asp:ListItem>
+                                    <asp:ListItem>Online</asp:ListItem>
+                                      <asp:ListItem>Offline</asp:ListItem>
+                                </asp:DropDownList>
+                                <asp:RequiredFieldValidator ID="reqfddlpaymentMethod" runat="server" ControlToValidate="ddlpaymentMethod" InitialValue="-Select-" ErrorMessage="*" ForeColor="Red" ValidationGroup="valGroupRegister"></asp:RequiredFieldValidator>
+                            </td>
+                            <td>&nbsp;</td>
+                        </tr>
+                        <tr>
+
+                            <td>On Credit</td>
+                            <td>
+
+                                <asp:CheckBox ID="chkOnCredit" runat="server" />
+
+                            </td>
+                            <td>&nbsp;</td>
+                            <tr>
+
+                                <td>Credit Limit</td>
+                                <td>
+
+                                    <asp:TextBox ID="txtCreditLimit" runat="server"></asp:TextBox>
+
+                                </td>
+                                <td>&nbsp;</td>
+                                <tr>
+
+                                    <td>&nbsp;</td>
+                                    <td>
+                                        <asp:Label ID="lbStatus" runat="server" Text=""></asp:Label>
+                                    </td>
+                                    <td>&nbsp;</td>
+
+                                </tr>
+                              
+                            </tr>
+                        </tr>
+                    </table>
+
+
     <table id="buttonsection" class="buttonsection">
         <tr>
             <td style="width: 74px; height: 26px">
--- ResortReservation/ResortManager/MasterUI/AgentMaster.aspx.cs
+++ ResortReservation/ResortManager/MasterUI/AgentMaster.aspx.cs
@@ -1,13 +1,36 @@
-using FarHorizon.Reservations.Bases.BasePages;
-using FarHorizon.Reservations.Common;
-using FarHorizon.Reservations.Common.DataEntities.Masters;
-using FarHorizon.Reservations.MasterServices;
 using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+
+using FarHorizon.Reservations.Common;
+using FarHorizon.Reservations.BusinessServices;
+using FarHorizon.Reservations.MasterServices;
+using FarHorizon.Reservations.Common.DataEntities.Masters;
+
+using FarHorizon.Reservations.Bases;
+using FarHorizon.Reservations.Bases.BasePages;
 
 public partial class MasterUI_AgentMaster : MasterBasePage
 {
+    BALAgentPayment blAgentpayment = new BALAgentPayment();
+    DALAgentPayment dlAgentpayment = new DALAgentPayment();
+    int GetQueryResponse = 0;
+    DataTable dtGetReturenedData;
+
+    BALLinks blLinks = new BALLinks();
+    DALLinks dlLinks = new DALLinks();
+    DataTable dtGetReturnedData;
+
+
     #region ControlsEvents
+
     protected void Page_Load(object sender, EventArgs e)
     {
         btnDelete.Attributes.Add("onclick", "return confirm('Are you sure you want to delete this record?')");
@@ -32,6 +55,8 @@ public partial class MasterUI_AgentMaster : MasterBasePage
         txtAgentName.Text = String.Empty;
         txtAgentCode.Text = String.Empty;
         txtAgentEmailId.Text = String.Empty;
+        txtPassword.Text = String.Empty;
+
     }
 
     protected void btnEdit_Click(object sender, EventArgs e)
@@ -44,11 +69,13 @@ public partial class MasterUI_AgentMaster : MasterBasePage
         {
             lblStatus.Text = "Add action initiated.";
             Save();
+
         }
         else
         {
             lblStatus.Text = "Update action initiated.";
             Update();
+
         }
         hfId.Value = "";
         RefreshGrid();
@@ -89,10 +116,39 @@ public partial class MasterUI_AgentMaster : MasterBasePage
             txtAgentCode.Text = oAgentData[0].AgentCode.ToString();
             txtAgentName.Text = oAgentData[0].AgentName.ToString();
             txtAgentEmailId.Text = oAgentData[0].EmailId.ToString();
+            //  txtPassword.TextMode =TextBoxMode.SingleLine ;
+            txtPassword.Text = oAgentData[0].Password.ToString();
+
+            //   txtPassword.TextMode = TextBoxMode.Password;
         }
         oAgentMaster = null;
         oAgentData = null;
 
+        #region fillingPaymentInfo
+        dtGetReturenedData = new DataTable();
+        blAgentpayment.agentid = iAgentID;
+        blAgentpayment._Action = "getPaymentInfo";
+        dtGetReturenedData = dlAgentpayment.GetAgentPaymentInfo(blAgentpayment);
+        if (dtGetReturenedData != null)
+        {
+            if (dtGetReturenedData.Rows.Count > 0)
+            {
+                txtBillingAddress.Text = dtGetReturenedData.Rows[0]["BillingAddress"].ToString();
+                txtCreditLimit.Text = dtGetReturenedData.Rows[0]["CreditLimit"].ToString();
+                chkOnCredit.Checked = Convert.ToBoolean(dtGetReturenedData.Rows[0]["OnCredit"].ToString() == "" ? "false" : dtGetReturenedData.Rows[0]["OnCredit"].ToString());
+                ddlpaymentMethod.SelectedValue = dtGetReturenedData.Rows[0]["PaymentMethod"].ToString();
+                txtPhone.Text = dtGetReturenedData.Rows[0]["phone"].ToString();
+            }
+            else
+            {
+                txtBillingAddress.Text = "";
+                txtCreditLimit.Text = "";
+                chkOnCredit.Checked = false;
+                ddlpaymentMethod.ClearSelection();
+            }
+
+        }
+        #endregion
         //btnAddNew.Enabled = false;
         //btnCancel.Enabled = true;
         btnDelete.Enabled = true;
@@ -100,6 +156,9 @@ public partial class MasterUI_AgentMaster : MasterBasePage
         btnEdit.Text = "Update";
         //btnSave.Enabled = false;
         lblStatus.Text = "";
+
+
+
     }
     protected void dgAgents_DeleteCommand(object source, DataGridCommandEventArgs e)
     {
@@ -127,16 +186,128 @@ public partial class MasterUI_AgentMaster : MasterBasePage
         oAgentData.AgentCode = Convert.ToString(txtAgentCode.Text.Trim());
         oAgentData.AgentName = Convert.ToString(txtAgentName.Text.Trim());
         oAgentData.EmailId = txtAgentEmailId.Text.Trim();
+        oAgentData.Password = txtPassword.Text.Trim();
         AgentMaster oAgentMaster = new AgentMaster();
         bActionCompleted = oAgentMaster.Insert(oAgentData);
         if (bActionCompleted == true)
         {
             base.DisplayAlert("The record has been inserted successfully");
+            PaymentInfo();
             ClearControls();
         }
         else
             lblStatus.Text = "Error Occured while insertion: Please refer to the error log.";
     }
+
+    private void PaymentInfo()
+    {
+        try
+        {
+            //dtGetReturenedData = new DataTable();
+            //dtGetReturenedData = dlAgentpayment.getagentmasterinfo(Convert.ToInt32(ddlAgent.SelectedValue));
+
+
+
+            blAgentpayment._Action = "AddDetails";
+            blAgentpayment._FirstName = txtAgentName.Text;
+            blAgentpayment._LastName = "";
+            blAgentpayment._PaymentMethod = ddlpaymentMethod.SelectedItem.Text;
+            blAgentpayment._EmailId = txtAgentEmailId.Text.Trim();
+            //   blAgentpayment._AgentCode = Convert.ToInt32(dtGetReturenedData.Rows[0]["AgentId"].ToString());
+            blAgentpayment._Password = txtPassword.Text.Trim();
+            blAgentpayment.Phone = txtPhone.Text.Trim();
+
+            blAgentpayment._BillingAddress = txtBillingAddress.Text.Trim().ToString();
+            blAgentpayment.OnCredit = chkOnCredit.Checked;
+            blAgentpayment.CreditLimit = Convert.ToDecimal(txtCreditLimit.Text.Trim() == "" ? "0" : txtCreditLimit.Text.Trim());
+
+
+            GetQueryResponse = dlAgentpayment.AddpaymentDetails(blAgentpayment);
+
+            if (GetQueryResponse > 0)
+            {
+                //  ClearAllControls();
+                lbStatus.Text = "Payment details saved successfully.";
+                lbStatus.ForeColor = System.Drawing.Color.Green;
+
+            }
+            else
+            {
+                lbStatus.Text = "Not Saved.. Please check ur entries once";
+                lbStatus.ForeColor = System.Drawing.Color.Red;
+            }
+
+
+
+        }
+        catch (Exception sqe)
+        {
+            lbStatus.Text = "Unexpected error found . please check ur entries.";
+            lbStatus.ForeColor = System.Drawing.Color.Red;
+        }
+
+    }
+
+    private void UpdatePaymentInfo()
+    {
+        try
+        {
+            //dtGetReturenedData = new DataTable();
+            //dtGetReturenedData = dlAgentpayment.getagentmasterinfo(Convert.ToInt32(ddlAgent.SelectedValue));
+            int Id;
+
+            int.TryParse(hfId.Value, out Id);
+
+            blAgentpayment._Action = "updatepaymenInfo";
+            blAgentpayment._FirstName = txtAgentName.Text;
+            blAgentpayment._LastName = "";
+            blAgentpayment._PaymentMethod = ddlpaymentMethod.SelectedItem.Text;
+            blAgentpayment._EmailId = txtAgentEmailId.Text.Trim();
+            blAgentpayment._AgentCode = Id;
+            blAgentpayment.Phone = txtPhone.Text.Trim();
+            if (txtPassword.Text != "")
+            {
+
+                blAgentpayment._Password = txtPassword.Text.Trim();
+            }
+            else
+            {
+                blAgentpayment._Password = null;
+            }
+
+
+            blAgentpayment._BillingAddress = txtBillingAddress.Text.Trim().ToString();
+            blAgentpayment.OnCredit = chkOnCredit.Checked;
+            blAgentpayment.CreditLimit = Convert.ToDecimal(txtCreditLimit.Text.Trim() == "" ? "0" : txtCreditLimit.Text.Trim());
+
+
+            GetQueryResponse = dlAgentpayment.UpdatepaymentDetails(blAgentpayment);
+
+            if (GetQueryResponse > 0)
+            {
+                //  ClearAllControls();
+                lbStatus.Text = "Payment details saved successfully.";
+                lbStatus.ForeColor = System.Drawing.Color.Green;
+
+            }
+            else
+            {
+                lbStatus.Text = "Not Saved.. Please check ur entries once";
+                lbStatus.ForeColor = System.Drawing.Color.Red;
+            }
+
+
+
+        }
+        catch (Exception sqe)
+        {
+            lbStatus.Text = "Unexpected error found . please check ur entries.";
+            lbStatus.ForeColor = System.Drawing.Color.Red;
+        }
+
+    }
+
+
     private void Update()
     {
         int Id = 0;
@@ -160,11 +331,21 @@ public partial class MasterUI_AgentMaster : MasterBasePage
         oAgentData.AgentName = txtAgentName.Text.Trim();
         oAgentData.AgentCode = txtAgentCode.Text.Trim();
         oAgentData.EmailId = txtAgentEmailId.Text.Trim();
+        if (txtPassword.Text != "")
+        {
+
+            oAgentData.Password = txtPassword.Text.Trim();
+        }
+        else
+        {
+            oAgentData.Password = null;
+        }
         AgentMaster oAgentMaster = new AgentMaster();
         bActionCompleted = oAgentMaster.Update(oAgentData);
         if (bActionCompleted == true)
         {
             base.DisplayAlert("The record has been updated successfully");
+            UpdatePaymentInfo();
             ClearControls();
         }
         else
@@ -285,4 +466,27 @@ public partial class MasterUI_AgentMaster : MasterBasePage
         return true;
     }
     #endregion UserDefinedFunctions
+    protected void dgAgents_ItemCommand(object source, DataGridCommandEventArgs e)
+    {
+        if (e.CommandName == "MaptoRate")
+        {
+            try
+            {
+
+                LinkButton lnk = (LinkButton)e.CommandSource;
+                DataGridItem gitem = (DataGridItem)lnk.NamingContainer;
+                int agentid = Convert.ToInt32(dgAgents.DataKeys[gitem.ItemIndex].ToString());
+
+                string url = "../Rate/MapAgentsToRate.aspx?agentId=" + agentid;
+                Response.Redirect(url);
+                //Response.Write("<script> window.open('" + url + "','_blank'); </script>");
+                // Page.ClientScript.RegisterStartupScript(this.GetType(), "popup", "<script language=javascript>window.open('" + url + "','','width=300px,height=200px').focus();</script>");
+                //ScriptManager.RegisterStartupScript(this, this.GetType(), "myFunction", "<script language=javascript> window.open('" + url + "','','width=300px,height=200px').focus();</script>", true);
+            }
+            catch
+            {
+            }
+
+        }
+    }
 }
